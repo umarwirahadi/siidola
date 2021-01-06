0d68ce61c7792a6e7630d6cd148e05e02b1470e1
umarwirahadi <u.wirahadi10@gmail.com>
2021-01-06 10:18:18 +0700

    update ignore file

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/siidola/';
-// $config['base_url'] = 'https://siidola.cimahikota.go.id';
+//$config['base_url'] = 'http://localhost/siidola/';
+ $config['base_url'] = 'https://siidola.cimahikota.go.id';
 
 /*
 |--------------------------------------------------------------------------
